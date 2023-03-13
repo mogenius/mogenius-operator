6b1ebc5c7cc38ddebe629eebc0d0d5867bc5700b
Benedikt Iltisberger <bene@mogenius.com>
2023-03-13 16:19:42 +0100

    fix: minor fixes.

--- version/consts.go
+++ version/consts.go
@@ -3,7 +3,7 @@ package version
 // IMPORTANT! DO NOT MODIFY THIS FILE BECAUSE SUBSCRIPTS ARE GOING TO FAIL IF YOU DO SO!
 
 var (
-	Ver            = "1.1.11"
+	Ver            = "1.1.12"
 	Branch         = ""
 	GitCommitHash  = "" // ldflags
 	BuildTimestamp = "" // ldflags
