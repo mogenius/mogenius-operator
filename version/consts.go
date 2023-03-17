1b978c882cb606389b0899f981e9f7466020166f
Benedikt Iltisberger <bene@mogenius.com>
2023-03-17 11:58:29 +0100

    fix: pipeline without azure dependency.

--- version/consts.go
+++ version/consts.go
@@ -3,7 +3,7 @@ package version
 // IMPORTANT! DO NOT MODIFY THIS FILE BECAUSE SUBSCRIPTS ARE GOING TO FAIL IF YOU DO SO!
 
 var (
-	Ver            = "1.2.8"
+	Ver            = "1.2.9"
 	Branch         = ""
 	GitCommitHash  = "" // ldflags
 	BuildTimestamp = "" // ldflags
