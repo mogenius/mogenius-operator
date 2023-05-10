abea5e1af21fd5b0972d6b26c1bd3235f7d45a2a
Benedikt Iltisberger <bene@mogenius.com>
2023-05-10 09:18:13 +0200

    fix: version bump for eri.

--- version/consts.go
+++ version/consts.go
@@ -3,7 +3,7 @@ package version
 // IMPORTANT! DO NOT MODIFY THIS FILE BECAUSE SUBSCRIPTS ARE GOING TO FAIL IF YOU DO SO!
 
 var (
-	Ver            = "1.5.3"
+	Ver            = "1.5.4"
 	Branch         = ""
 	GitCommitHash  = "" // ldflags
 	BuildTimestamp = "" // ldflags
