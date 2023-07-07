f634c59e7acf1e9c4bf7be6d3e0fa60514c06fdb
Benedikt Iltisberger <bene@mogenius.com>
2023-07-07 10:07:55 +0200

    fix: nfs minor fix.

--- version/consts.go
+++ version/consts.go
@@ -2,7 +2,7 @@ package version
 
 // IMPORTANT! DO NOT MODIFY THIS FILE BECAUSE SUBSCRIPTS ARE GOING TO FAIL IF YOU DO SO!
 var (
-	Ver            = "1.10.19"
+	Ver            = "1.10.20"
 	Branch         = ""
 	GitCommitHash  = "" // ldflags
 	BuildTimestamp = "" // ldflags
