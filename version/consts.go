dbe9b8ba7bd9d37d020bb93fcf2f81dd913d72a5
Benedikt Iltisberger <bene@mogenius.com>
2023-07-12 13:56:46 +0200

    fix: new certificate handling.

--- version/consts.go
+++ version/consts.go
@@ -2,7 +2,7 @@ package version
 
 // IMPORTANT! DO NOT MODIFY THIS FILE BECAUSE SUBSCRIPTS ARE GOING TO FAIL IF YOU DO SO!
 var (
-	Ver            = "1.12.2"
+	Ver            = "1.12.3"
 	Branch         = ""
 	GitCommitHash  = "" // ldflags
 	BuildTimestamp = "" // ldflags
