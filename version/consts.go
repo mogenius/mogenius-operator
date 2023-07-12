deb300645ef22e46ba530560eaca82aac128b073
Benedikt Iltisberger <bene@mogenius.com>
2023-07-12 15:03:38 +0200

    fix: more rbac permissions for monitoring.

--- version/consts.go
+++ version/consts.go
@@ -2,7 +2,7 @@ package version
 
 // IMPORTANT! DO NOT MODIFY THIS FILE BECAUSE SUBSCRIPTS ARE GOING TO FAIL IF YOU DO SO!
 var (
-	Ver            = "1.12.7"
+	Ver            = "1.12.8"
 	Branch         = ""
 	GitCommitHash  = "" // ldflags
 	BuildTimestamp = "" // ldflags
