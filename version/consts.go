6dda62187d8d5c5a6088460fdcff6ad8afd9c16b
Benedikt Iltisberger <bene@mogenius.com>
2023-07-14 13:40:37 +0200

    fix: massive refactoring.

--- version/consts.go
+++ version/consts.go
@@ -2,7 +2,7 @@ package version
 
 // IMPORTANT! DO NOT MODIFY THIS FILE BECAUSE SUBSCRIPTS ARE GOING TO FAIL IF YOU DO SO!
 var (
-	Ver            = "1.12.13"
+	Ver            = "1.12.14"
 	Branch         = ""
 	GitCommitHash  = "" // ldflags
 	BuildTimestamp = "" // ldflags
