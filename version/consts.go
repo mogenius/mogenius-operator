3985bc26145f1283f3b984cb79d73bda44150b44
Benedikt Iltisberger <bene@mogenius.com>
2023-07-27 14:37:25 +0200

    fix: builder updates.

--- version/consts.go
+++ version/consts.go
@@ -2,7 +2,7 @@ package version
 
 // IMPORTANT! DO NOT MODIFY THIS FILE BECAUSE SUBSCRIPTS ARE GOING TO FAIL IF YOU DO SO!
 var (
-	Ver            = "1.13.3"
+	Ver            = "1.13.4"
 	Branch         = ""
 	GitCommitHash  = "" // ldflags
 	BuildTimestamp = "" // ldflags
