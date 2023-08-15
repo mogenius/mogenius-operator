54f757352aedeb5f5cfb812d46d28e2c379f714c
Benedikt Iltisberger <bene@mogenius.com>
2023-08-15 15:31:00 +0200

    fix: docker-desktop, k3s storage support.

--- version/consts.go
+++ version/consts.go
@@ -2,7 +2,7 @@ package version
 
 // IMPORTANT! DO NOT MODIFY THIS FILE BECAUSE SUBSCRIPTS ARE GOING TO FAIL IF YOU DO SO!
 var (
-	Ver            = "1.14.4"
+	Ver            = "1.14.5"
 	Branch         = ""
 	GitCommitHash  = "" // ldflags
 	BuildTimestamp = "" // ldflags
