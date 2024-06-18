0adca8e9d0c6566a3693ed6625891c2d930140d5
Valentin <valentin@mogenius.com>
2024-06-18 10:42:12 +0200

    added tests for secret store functions

--- kubernetes/external-secrets-store.go
+++ kubernetes/external-secrets-store.go
@@ -2,10 +2,43 @@ package kubernetes
 
 import (
 	"mogenius-k8s-manager/utils"
+	"strings"
 )
 
-func CreateExternalSecretsStore() {
-	// CreateOrUpdateYamlString(utils.InitExternalSecretsStoreYaml())
-	ApplyResource(utils.InitExternalSecretsStoreYaml(), true)
+type ExternalSecretStoreProps struct {
+	Role           string
+	VaultServerUrl string
+	MoSharedPath   string
+	ServiceAccount string
+}
 
+// NewExternalSecretStore creates a new SecretStore with default values.
+func NewExternalSecretStore() *ExternalSecretStoreProps {
+	return &ExternalSecretStoreProps{
+		Role:           "mogenius-external-secrets",
+		VaultServerUrl: "http://vault.default.svc.cluster.local:8200",
+		MoSharedPath:   "secret/mogenius-external-secrets",
+		ServiceAccount: "external-secrets-sa",
+	}
+}
+
+func CreateExternalSecretsStore() {
+	props := NewExternalSecretStore()
+
+	ApplyResource(
+		renderClusterSecretStore(
+			utils.InitExternalSecretsStoreYaml(),
+			*props,
+		),
+		true,
+	)
+}
+
+func renderClusterSecretStore(yamlTemplateString string, props ExternalSecretStoreProps) string {
+	yamlTemplateString = strings.Replace(yamlTemplateString, "<MO_SHARED_PATH>", props.MoSharedPath, -1)
+	yamlTemplateString = strings.Replace(yamlTemplateString, "<VAULT_SERVER_URL>", props.VaultServerUrl, -1)
+	yamlTemplateString = strings.Replace(yamlTemplateString, "<ROLE>", props.Role, -1)
+	yamlTemplateString = strings.Replace(yamlTemplateString, "<SERVICE_ACC>", props.ServiceAccount, -1)
+
+	return yamlTemplateString
 }
--- kubernetes/external-secrets-store_test.go
+++ kubernetes/external-secrets-store_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/mogenius/punq/logger"
+	"gopkg.in/yaml.v2"
 )
 
 func TestSecretStoreResource(t *testing.T) {
@@ -62,4 +63,61 @@ spec:
 	} else {
 		logger.Log.Info("Resource deleted ✅")
 	}
+}
+
+func TestSecretStoreRender(t *testing.T) {
+
+	yamlTemplate := `apiVersion: external-secrets.io/v1beta1
+kind: ClusterSecretStore
+metadata:
+  name: secret-store-vault-role-based
+  annotations:
+    mogenius-external-secrets/shared-path: <MO_SHARED_PATH>
+spec:
+  provider:
+    vault:
+      server: <VAULT_SERVER_URL>
+      version: "v2"
+      auth:
+        kubernetes:
+         mountPath: "kubernetes"
+         role: <ROLE>
+         serviceAccountRef:
+           name: <SERVICE_ACC>
+`
+
+	secretStore := NewExternalSecretStore()
+	secretStore.Role = "mo-external-secrets-002"
+	yamlDataUpdated := renderClusterSecretStore(yamlTemplate, *secretStore)
+
+	if yamlTemplate == yamlDataUpdated {
+		t.Errorf("Error updating yaml data: %s", yamlTemplate)
+	} else {
+		logger.Log.Info("Yaml data updated ✅")
+	}
+
+	expectedPath := "secret/mo-ex-secr-test-003"
+	secretStore.MoSharedPath = expectedPath
+	yamlDataUpdated = renderClusterSecretStore(yamlTemplate, *secretStore)
+
+	// check if the values are replaced
+	var data YamlData
+	err := yaml.Unmarshal([]byte(yamlDataUpdated), &data)
+	if err != nil {
+		t.Fatalf("Error parsing YAML: %v", err)
+	}
+
+	if data.Metadata.Annotations.SharedPath != expectedPath {
+		t.Errorf("Error updating MoSharedPath: expected: %s, got: %s", expectedPath, data.Metadata.Annotations.SharedPath)
+	} else {
+		logger.Log.Info("MoSharedPath updated ✅")
+	}
+}
+
+type YamlData struct {
+	Metadata struct {
+		Annotations struct {
+			SharedPath string `yaml:"mogenius-external-secrets/shared-path"`
+		} `yaml:"annotations"`
+	} `yaml:"metadata"`
 }
