455b28bc82d9e37d79f7999f04dc66c09720ef43
Valentin <valentin@mogenius.com>
2024-06-20 10:31:52 +0200

    init

---
+++ services/external-secrets.go
@@ -0,0 +1,150 @@
+package services
+
+import (
+	"encoding/json"
+	"fmt"
+	mokubernetes "mogenius-k8s-manager/kubernetes"
+	"mogenius-k8s-manager/utils"
+
+	"strings"
+
+	"github.com/mogenius/punq/logger"
+)
+
+type ExternalSecretStoreProps struct {
+	CreateSecretsStoreRequest
+	Name           string
+	ServiceAccount string
+}
+
+func externalSecretStoreExample() *ExternalSecretStoreProps {
+	return NewExternalSecretStore(CreateSecretsStoreRequestExample())
+}
+
+// NewExternalSecretStore creates a new NewExternalSecretStore with default values.
+func NewExternalSecretStore(data CreateSecretsStoreRequest) *ExternalSecretStoreProps {
+	return &ExternalSecretStoreProps{
+		CreateSecretsStoreRequest: data,
+		Name:                      data.NamePrefix + "-vault-secret-store",
+		ServiceAccount:            "external-secrets-sa",
+	}
+}
+
+func CreateExternalSecretsStore(data CreateSecretsStoreRequest) CreateSecretsStoreResponse {
+	props := NewExternalSecretStore(data)
+
+	mokubernetes.CreateServiceAccount(props.ServiceAccount, utils.CONFIG.Kubernetes.OwnNamespace)
+
+	err := mokubernetes.ApplyResource(
+		renderClusterSecretStore(
+			utils.InitExternalSecretsStoreYaml(),
+			*props,
+		),
+		true,
+	)
+	if err != nil {
+		return CreateSecretsStoreResponse{
+			Status: "ERROR",
+		}
+	} else {
+		return CreateSecretsStoreResponse{
+			Status: "SUCCESS",
+		}
+	}
+}
+
+func ListExternalSecretsStores() ListSecretsStoresResponse {
+	// LIST
+	response, err := mokubernetes.ListResources("external-secrets.io", "v1beta1", "clustersecretstores", "", true)
+	if err != nil {
+		logger.Log.Info("ListResources failed")
+	}
+
+	jsonOutput, err := json.MarshalIndent(response, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+		return ListSecretsStoresResponse{}
+	}
+	stores, err := parseSecretStoresListing(string(jsonOutput))
+	if err != nil {
+		fmt.Println(err)
+		return ListSecretsStoresResponse{}
+	}
+
+	return ListSecretsStoresResponse{
+		StoresInCluster: stores,
+	}
+}
+
+func DeleteExternalSecretsStore(data DeleteSecretsStoreRequest) DeleteSecretsStoreResponse {
+
+	err := mokubernetes.DeleteResource("external-secrets.io", "v1beta1", "clustersecretstores", data.Name, "", true)
+	if err != nil {
+		return DeleteSecretsStoreResponse{
+			Status: "ERROR",
+		}
+	} else {
+		return DeleteSecretsStoreResponse{
+			Status: "SUCCESS",
+		}
+	}
+}
+
+func renderClusterSecretStore(yamlTemplateString string, props ExternalSecretStoreProps) string {
+	yamlTemplateString = strings.Replace(yamlTemplateString, "<VAULT_STORE_NAME>", props.Name, -1)
+	yamlTemplateString = strings.Replace(yamlTemplateString, "<MO_SHARED_PATH>", props.MoSharedPath, -1)
+	yamlTemplateString = strings.Replace(yamlTemplateString, "<VAULT_SERVER_URL>", props.VaultServerUrl, -1)
+	yamlTemplateString = strings.Replace(yamlTemplateString, "<ROLE>", props.Role, -1)
+	yamlTemplateString = strings.Replace(yamlTemplateString, "<SERVICE_ACC>", props.ServiceAccount, -1)
+
+	return yamlTemplateString
+}
+
+type SecretStoreListing struct {
+	Name    string `json:"name"`
+	Role    string `json:"role"`
+	Message string `json:"message"`
+}
+
+type SecretStoreListingSchema struct {
+	Items []struct {
+		Metadata struct {
+			Name string `json:"name"`
+		} `json:"metadata"`
+		Spec struct {
+			Provider struct {
+				Vault struct {
+					Auth struct {
+						Kubernetes struct {
+							Role string `json:"role"`
+						} `json:"kubernetes"`
+					} `json:"auth"`
+				} `json:"vault"`
+			} `json:"provider"`
+		} `json:"spec"`
+		Status struct {
+			Conditions []struct {
+				Message string `json:"message"`
+			} `json:"conditions"`
+		} `json:"status"`
+	} `json:"items"`
+}
+
+func parseSecretStoresListing(jsonStr string) ([]SecretStoreListing, error) {
+	var secretStores SecretStoreListingSchema
+	err := json.Unmarshal([]byte(jsonStr), &secretStores)
+	if err != nil {
+		return nil, err
+	}
+
+	var stores []SecretStoreListing
+	for _, item := range secretStores.Items {
+		store := SecretStoreListing{
+			Name:    item.Metadata.Name,
+			Role:    item.Spec.Provider.Vault.Auth.Kubernetes.Role,
+			Message: item.Status.Conditions[0].Message,
+		}
+		stores = append(stores, store)
+	}
+	return stores, nil
+}
