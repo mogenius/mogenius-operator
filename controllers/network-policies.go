3e130b8d0919b8bb50fe7de2d34bc383d3332e8b
mogenius git-user <git@mogenius.com>
2024-09-24 14:38:36 +0200

    chore: typo

--- controllers/network-policies.go
+++ controllers/network-policies.go
@@ -4,7 +4,7 @@ import "mogenius-k8s-manager/dtos"
 
 type CreateLabeledNetworkPolicyRequest struct {
 	Namespace                  dtos.K8sNamespaceDto               `json:"namespace" validate:"required"`
-	LabeledNetworkPolicyParams dtos.K8sLabeledNetworkPolicyParams `json: "labeledNetworkPolicyParams" validate:"required"`
+	LabeledNetworkPolicyParams dtos.K8sLabeledNetworkPolicyParams `json:"labeledNetworkPolicyParams" validate:"required"`
 }
 
 type CreateLabeledNetworkPolicyResponse struct {
