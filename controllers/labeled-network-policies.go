8e1863ad19ebc5730d06fa24cc520f1b146b0ade
Behrang Alavi <behrang@mogenius.com>
2024-10-16 21:25:20 +0200

    fix: refactoring network policy response

--- controllers/labeled-network-policies.go
+++ controllers/labeled-network-policies.go
@@ -13,10 +13,10 @@ const (
 	failure status = "failure"
 )
 
-type DetachLabeledNetworkPolicyResponse struct {
-	Status  status `json:"status"`
-	Message string `json:"message"`
-}
+//	type DetachLabeledNetworkPolicyResponse struct {
+//		Status  status `json:"status"`
+//		Message string `json:"message"`
+//	}
 type DetachLabeledNetworkPolicyRequest struct {
 	ControllerName       string                          `json:"controllerName" validate:"required"`
 	ControllerType       dtos.K8sServiceControllerEnum   `json:"controllerType" validate:"required"`
@@ -26,18 +26,20 @@ type DetachLabeledNetworkPolicyRequest struct {
 
 type LabeledNetworkPoliciesListResponse []dtos.K8sLabeledNetworkPolicyDto
 
-func DetachLabeledNetworkPolicy(data DetachLabeledNetworkPolicyRequest) DetachLabeledNetworkPolicyResponse {
+func DetachLabeledNetworkPolicy(data DetachLabeledNetworkPolicyRequest) (string, error) {
 	err := kubernetes.DetachLabeledNetworkPolicy(data.ControllerName, data.ControllerType, data.NamespaceName, data.LabeledNetworkPolicy)
 	if err != nil {
-		return DetachLabeledNetworkPolicyResponse{
-			Status:  failure,
-			Message: fmt.Sprintf("Failed to detach network policy, err: %v", err.Error()),
-		}
+		return "", fmt.Errorf("Failed to detach network policy, err: %v", err.Error())
+		//return DetachLabeledNetworkPolicyResponse{
+		//	Status:  failure,
+		//	Message: fmt.Sprintf("Failed to detach network policy, err: %v", err.Error()),
+		//}
 	}
 
-	return DetachLabeledNetworkPolicyResponse{
-		Status: success,
-	}
+	return "", nil
+	//return DetachLabeledNetworkPolicyResponse{
+	//	Status: success,
+	//}
 }
 
 type AttachLabeledNetworkPolicyRequest struct {
@@ -47,30 +49,33 @@ type AttachLabeledNetworkPolicyRequest struct {
 	LabeledNetworkPolicy dtos.K8sLabeledNetworkPolicyDto `json:"labeledNetworkPolicy" validate:"required"`
 }
 
-type AttachLabeledNetworkPolicyResponse struct {
-	Status  status `json:"status"`
-	Message string `json:"message"`
-}
+//type AttachLabeledNetworkPolicyResponse struct {
+//	Status  status `json:"status"`
+//	Message string `json:"message"`
+//}
 
-func AttachLabeledNetworkPolicy(data AttachLabeledNetworkPolicyRequest) AttachLabeledNetworkPolicyResponse {
+func AttachLabeledNetworkPolicy(data AttachLabeledNetworkPolicyRequest) (string, error) {
 	err := kubernetes.EnsureLabeledNetworkPolicy(data.NamespaceName, data.LabeledNetworkPolicy)
 	if err != nil {
-		return AttachLabeledNetworkPolicyResponse{
-			Status:  failure,
-			Message: fmt.Sprintf("Failed to create network policy, err: %v", err.Error()),
-		}
+		return "", fmt.Errorf("Failed to create network policy, err: %v", err.Error())
+		//return AttachLabeledNetworkPolicyResponse{
+		//	Status:  failure,
+		//	Message: fmt.Sprintf("Failed to create network policy, err: %v", err.Error()),
+		//}
 	}
 	err = kubernetes.AttachLabeledNetworkPolicy(data.ControllerName, data.ControllerType, data.NamespaceName, data.LabeledNetworkPolicy)
 	if err != nil {
-		return AttachLabeledNetworkPolicyResponse{
-			Status:  failure,
-			Message: fmt.Sprintf("Failed to attach network policy, err: %v", err.Error()),
-		}
+		return "", fmt.Errorf("Failed to attach network policy, err: %v", err.Error())
+		//return AttachLabeledNetworkPolicyResponse{
+		//	Status:  failure,
+		//	Message: fmt.Sprintf("Failed to attach network policy, err: %v", err.Error()),
+		//}
 	}
 
-	return AttachLabeledNetworkPolicyResponse{
-		Status: success,
-	}
+	return "", nil
+	//return AttachLabeledNetworkPolicyResponse{
+	//	Status: success,
+	//}
 }
 
 func ListLabeledNetworkPolicyPortsExample() LabeledNetworkPoliciesListResponse {
@@ -98,21 +103,23 @@ type RemoveConflictingNetworkPoliciesRequest struct {
 	NamespaceName string `json:"namespaceName" validate:"required"`
 }
 
-type RemoveConflictingNetworkPoliciesResponse struct {
-	Status  status `json:"status"`
-	Message string `json:"message"`
-}
+//type RemoveConflictingNetworkPoliciesResponse struct {
+//	Status  status `json:"status"`
+//	Message string `json:"message"`
+//}
 
-func RemoveConflictingNetworkPolicies(data RemoveConflictingNetworkPoliciesRequest) RemoveConflictingNetworkPoliciesResponse {
+func RemoveConflictingNetworkPolicies(data RemoveConflictingNetworkPoliciesRequest) (string, error) {
 	err := kubernetes.RemoveAllNetworkPolicies(data.NamespaceName)
 	if err != nil {
-		return RemoveConflictingNetworkPoliciesResponse{
-			Status:  failure,
-			Message: fmt.Sprintf("Failed to delete all network policies, err: %v", err.Error()),
-		}
+		return "", fmt.Errorf("Failed to delete all network policies, err: %v", err.Error())
+		//return RemoveConflictingNetworkPoliciesResponse{
+		//	Status:  failure,
+		//	Message: fmt.Sprintf("Failed to delete all network policies, err: %v", err.Error()),
+		//}
 	}
 
-	return RemoveConflictingNetworkPoliciesResponse{
-		Status: success,
-	}
+	return "", nil
+	//return RemoveConflictingNetworkPoliciesResponse{
+	//	Status: success,
+	//}
 }
