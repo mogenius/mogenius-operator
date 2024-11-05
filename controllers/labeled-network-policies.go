9d6e267ad8c4867a0d35b0e7c9cfd989a1193015
eriandreas <andreas@mogenius.com>
2024-11-05 08:37:20 +0100

    fix: add comment

--- controllers/labeled-network-policies.go
+++ controllers/labeled-network-policies.go
@@ -261,6 +261,7 @@ func createNetworkPolicyDto(name string, spec v1.NetworkPolicySpec) dtos.K8sLabe
 }
 
 func ListAllNetworkPolicies() ([]ListNetworkPolicyNamespace, error) {
+	//
 	namespaces, err := kubernetes.ListAllNamespaces()
 	if err != nil {
 		return nil, fmt.Errorf("failed to list all namespaces, err: %s", err.Error())
