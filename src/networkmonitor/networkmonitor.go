95e68f1515d613bb98df65f31a63c78e6a59d9ca
Benedikt Iltisberger <bene@mogenius.com>
2025-04-08 21:41:04 +0200

    fix: added assert again because it was very usefull.

--- src/networkmonitor/networkmonitor.go
+++ src/networkmonitor/networkmonitor.go
@@ -269,11 +269,7 @@ func (self *networkMonitor) updateCollectedStats(
 							}
 						}
 					}
-					if rootInterface == nil {
-						self.logger.Warn("failed to find root interface for virtual interface", "virtualInterface.ifName", virtualInterface.Ifname, "pod", pod.GetName())
-						continue
-					}
-					//assert.Assert(rootInterface != nil, "the root index has to be resolved succesfully")
+					assert.Assert(rootInterface != nil, "the root index has to be resolved succesfully")
 
 					count, ok := lastInterfaceData[rootInterface.Ifindex]
 					if !ok {
