cac0c8a46b3aad426d085e5cc5badde3d51e0fc8
Lukas Hankeln <lukashankeln@googlemail.com>
2026-04-28 15:22:28 +0200

    feat: remove obsolete reconcilers

--- src/reconciler/workspaces.go
+++ src/reconciler/workspaces.go
@@ -4,31 +4,13 @@ import (
 	"context"
 	"fmt"
 	"mogenius-operator/src/crds/v1alpha1"
-	"mogenius-operator/src/store"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
 func (d *reconcilerModule) reconcileWorkspaces(ctx context.Context, obj *unstructured.Unstructured, op operation) []ReconcileResult {
-	namespace := d.config.Get("MO_OWN_NAMESPACE")
-	grants, err := store.GetAllGrants(namespace)
-	if err != nil {
-		return []ReconcileResult{{Err: fmt.Errorf("failed to fetch grants for workspace reconciliation: %w", err)}}
-	}
-
 	var results []ReconcileResult
-	for _, grant := range grants {
-		if grant.Spec.TargetName != obj.GetName() || grant.Spec.TargetType != "workspace" {
-			continue
-		}
-		if op == deleteOperation {
-			// Workspace deleted: clean up all grants targeting it.
-			results = append(results, d.deleteGrantBindings(ctx, grant)...)
-		} else {
-			results = append(results, d.reconcileGrantInternal(ctx, grant)...)
-		}
-	}
 
 	if op != deleteOperation {
 		results = append(results, d.verifyWorkspaceIntegrity(ctx, obj)...)
